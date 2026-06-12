d854a527a793b200998e9f7f8e0d52e4b374a74e
0007Yash <yashuuu0000001@gmail.com>
2026-06-12 16:05:46 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,8 @@
-mport React from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 
-const container = document.getElementById("root");
-
-if (!container) {
-  throw new Error('Root element "#root" not found');
-}
-
-ReactDOM.createRoot(container).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
